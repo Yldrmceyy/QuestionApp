9a0477d3aa51b1c9983d0c33fd1a76f3826a9a76
Ceylan Yıldırım <yldrmceylan.1@gmail.com>
2024-08-28 18:39:38 +0300

    created function for start quiz

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,19 @@ import Introduction from "./components/introduction";
 import Quiz from "./components/Quiz";
 
 function App() {
+  const [isQuizStarted, setIsQuizStarted] = useState(false);
 
+  // Quiz'i başlatmak için kullanılan fonksiyon
+  const startQuiz = () => {
+    setIsQuizStarted(true);
+  };
 
-
-
-  return <>
-    <Introduction />
-    <Quiz />
-  </>;
+  return (
+    <>
+      <Introduction startQuiz={startQuiz} />
+      <Quiz />
+    </>
+  );
 }
 
 export default App;
