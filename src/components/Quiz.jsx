a41ee61f07e30a0d8183c6af204c3be06e5bbdd7
Ceylan Yıldırım <yldrmceylan.1@gmail.com>
2024-08-28 21:02:28 +0300

    showed quiz-content

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,15 +1,23 @@
-import { useState ,useEffect} from "react";
+import { useState, useEffect } from "react";
 import QUESTIONS from "../questions";
 
 export default function Quiz() {
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [userSelected, setUserSelected] = useState(null);
+  
 
   const currentQuestion = QUESTIONS[currentQuestionIndex];
 
+  return(
 
-  return <div>
-  
-  
-  
-  </div>;
+<div className="quiz-content">
+<h2>{currentQuestion.question}</h2>
+<img src={currentQuestion.media} alt="Question media" />
+<div className="options">
+
+</div>
+</div>
+
+    
+  ) 
 }
