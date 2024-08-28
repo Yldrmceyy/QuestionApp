7d2c8fd882196d324dc103f867accde20c69bb76
Ceylan Yıldırım <yldrmceylan.1@gmail.com>
2024-08-28 21:07:49 +0300

    handlenextquestion created

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -4,20 +4,34 @@ import QUESTIONS from "../questions";
 export default function Quiz() {
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [userSelected, setUserSelected] = useState(null);
-  
+  const [timeLeft, setTimeLeft] = useState(30);
+  const [score, setScore] = useState(0);
+
+
 
   const currentQuestion = QUESTIONS[currentQuestionIndex];
 
-  return(
+  // useEffect(()=>{
+  //   if()
+  // })
 
-<div className="quiz-content">
-<h2>{currentQuestion.question}</h2>
-<img src={currentQuestion.media} alt="Question media" />
-<div className="options">
+  const handleNextQuestion = () => {
+    setUserSelected(null);
+    setTimeLeft(30);
+    if (currentQuestionIndex < QUESTIONS.length - 1) {
+      setCurrentQuestionIndex(currentQuestionIndex + 1);
+    } else {
+      alert(`Test bitti! Doğru sayısı: ${score}`);
+      // Quiz bittiğinde sonucu gösterebilir veya başka bir işlem yapabilirsiniz
+    }
+  };
 
-</div>
-</div>
 
-    
-  ) 
+  return (
+    <div className="quiz-content">
+      <h2>{currentQuestion.question}</h2>
+      <img src={currentQuestion.media} alt="Question media" />
+      <div className="options"></div>
+    </div>
+  );
 }
